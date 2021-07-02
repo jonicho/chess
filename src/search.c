85ad48002f9ff642a4659db28a6bf290629c754c
Jonas Richard Keller <jonas.r.keller@gmail.com>
2021-07-02 19:57:54 +0200

    Improve search

--- src/search.c
+++ src/search.c
@@ -8,48 +8,65 @@
 static int minimax_alpha_beta(const Position *position, Move *best_move,
 			      size_t depth, int alpha, int beta)
 {
+	if (depth == 0) {
+		return eval_position(position);
+	}
+
 	Move moves[MAX_NUM_PSEUDO_LEGAL_MOVES];
 	size_t num_moves = gen_moves(moves, position);
 	size_t best_move_index = -1;
-	int best_move_eval =
-		position->side_to_move == WHITE ? INT_MIN : INT_MAX;
-	for (size_t i = 0; i < num_moves; i++) {
-		Position temp_position = *position;
-		make_move(&temp_position, moves[i]);
-		if (is_king_in_check(&temp_position,
-				     BLACK ^ temp_position.side_to_move)) {
-			continue;
-		}
-		int eval;
-		if (depth > 1) {
-			Move dummy_move;
-			if (position->side_to_move == WHITE) {
-				eval = minimax_alpha_beta(&temp_position,
-							  &dummy_move,
-							  depth - 1,
-							  best_move_eval, beta);
-			} else {
-				eval = minimax_alpha_beta(&temp_position,
-							  &dummy_move,
-							  depth - 1, alpha,
-							  best_move_eval);
+	int best_move_eval;
+	if (position->side_to_move == WHITE) {
+		best_move_eval = INT_MIN;
+		for (size_t i = 0; i < num_moves; i++) {
+			Position temp_position = *position;
+			make_move(&temp_position, moves[i]);
+			if (is_king_in_check(
+				    &temp_position,
+				    BLACK ^ temp_position.side_to_move)) {
+				continue;
 			}
-		} else {
-			eval = eval_position(&temp_position);
-		}
+			Move dummy_move;
+			int eval = minimax_alpha_beta(&temp_position,
+						      &dummy_move, depth - 1,
+						      best_move_eval, beta);
 
-		if ((position->side_to_move == WHITE &&
-		     eval > best_move_eval) ||
-		    (position->side_to_move == BLACK &&
-		     eval < best_move_eval)) {
-			best_move_eval = eval;
-			best_move_index = i;
+			if (eval > best_move_eval) {
+				best_move_eval = eval;
+				best_move_index = i;
+			}
+			if (eval > alpha) {
+				alpha = eval;
+			}
+			if (beta <= alpha) {
+				break;
+			}
 		}
-		if ((position->side_to_move == WHITE &&
-		     best_move_eval >= beta) ||
-		    (position->side_to_move == BLACK &&
-		     best_move_eval <= alpha)) {
-			break;
+	} else {
+		best_move_eval = INT_MAX;
+		for (size_t i = 0; i < num_moves; i++) {
+			Position temp_position = *position;
+			make_move(&temp_position, moves[i]);
+			if (is_king_in_check(
+				    &temp_position,
+				    BLACK ^ temp_position.side_to_move)) {
+				continue;
+			}
+			Move dummy_move;
+			int eval = minimax_alpha_beta(&temp_position,
+						      &dummy_move, depth - 1,
+						      alpha, best_move_eval);
+
+			if (eval < best_move_eval) {
+				best_move_eval = eval;
+				best_move_index = i;
+			}
+			if (eval < beta) {
+				beta = eval;
+			}
+			if (beta <= alpha) {
+				break;
+			}
 		}
 	}
 	*best_move = moves[best_move_index];
