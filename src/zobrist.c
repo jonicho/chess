aaf97fd0dc5ebf82eda53c9136430cc735dc9d81
Jonas Richard Keller <jonas.r.keller@gmail.com>
2022-07-21 16:10:41 +0200

    Use pcg random number generator instead of rand() to get better random numbers for zobrist hashing

--- src/zobrist.c
+++ src/zobrist.c
@@ -1,37 +1,34 @@
 #include "zobrist.h"
 
+#include "pcg/pcg_basic.h"
+
 #include <stddef.h>
-#include <stdlib.h>
 
 uint64_t zobrist_piece_hashes[120][16];
 uint64_t zobrist_black_to_move_hash;
 uint64_t zobrist_castling_ability_hashes[16];
 uint64_t zobrist_en_passant_target_square_hashes[8];
 
-static uint64_t rand_uint64()
+static uint64_t rand_uint64(pcg32_random_t *rng)
 {
-	uint64_t r = 0;
-	for (size_t i = 0; i < 64; i++) {
-		r <<= 1;
-		r |= rand() & 1;
-	}
-	return r;
+	return (uint64_t)pcg32_random_r(rng) << 32 | pcg32_random_r(rng);
 }
 
 void zobrist_init(unsigned int seed)
 {
-	srand(seed);
+	pcg32_random_t rng;
+	pcg32_srandom_r(&rng, seed, 0);
 	for (size_t square = 0; square < 120; square++) {
 		for (size_t piece = 0; piece < 16; piece++) {
-			zobrist_piece_hashes[square][piece] = rand_uint64();
+			zobrist_piece_hashes[square][piece] = rand_uint64(&rng);
 		}
 	}
-	zobrist_black_to_move_hash = rand_uint64();
+	zobrist_black_to_move_hash = rand_uint64(&rng);
 	for (size_t file = 0; file < 8; file++) {
-		zobrist_en_passant_target_square_hashes[file] = rand_uint64();
+		zobrist_en_passant_target_square_hashes[file] = rand_uint64(&rng);
 	}
 	for (size_t i = 0; i < 16; i++) {
-		zobrist_castling_ability_hashes[i] = rand_uint64();
+		zobrist_castling_ability_hashes[i] = rand_uint64(&rng);
 	}
 }
 
